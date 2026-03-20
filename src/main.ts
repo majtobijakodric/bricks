d47a43a97ce6381e0aab036aa04ce72f06b405b9
Maj <159164690+majtobijakodric@users.noreply.github.com>
2026-03-20 21:14:31 +0100

    Fresh start in ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,3 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
- <div id="main">
-  <div id="score"></div>
-  <div id="game">
-    <canvas id="canvas" width="400" height="400"></canvas>
-  </div>
- </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = ''
