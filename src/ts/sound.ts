574c0c0306592161642ab6704f3daee81a5f3afe
Maj <159164690+majtobijakodric@users.noreply.github.com>
2026-03-29 22:31:20 +0200

    Fixed brick sound not working before pad movement

--- src/ts/sound.ts
+++ src/ts/sound.ts
@@ -4,9 +4,7 @@ const brickHitSound = new Audio(brickHitSoundFile);
 brickHitSound.preload = 'auto';
 
 export function playBrickHitSound() {
-  const soundInstance = brickHitSound.cloneNode() as HTMLAudioElement;
-
-  soundInstance.currentTime = 0;
+  const soundInstance = new Audio(brickHitSoundFile);
   void soundInstance.play().catch(() => {
   });
 }
